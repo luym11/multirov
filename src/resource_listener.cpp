24b2e9609dead6776f96d9d7326090f38ea54ed2
yimeng_sp <lym602@126.com>
2017-07-28 13:35:41 +0300

    Added resource vehicle: listen to its tf

---
+++ src/resource_listener.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/Twist.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "my_tf_listener");
+
+  ros::NodeHandle node;
+
+  //ros::Publisher turtle_vel =
+ //   node.advertise<geometry_msgs::Twist>("turtle2/cmd_vel", 10);
+
+  tf::TransformListener listener;
+
+  ros::Rate rate(10.0);
+  while (node.ok()){
+    tf::StampedTransform transform;
+    try{ 
+      listener.lookupTransform("/world", "/rexrov0/base_stabilized", ros::Time(0), transform);
+    }
+    catch (tf::TransformException &ex) {
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+      continue;
+    }
+
+    geometry_msgs::Twist vel_msg;
+    vel_msg.angular.z = 4.0 * atan2(transform.getOrigin().y(),
+                                    transform.getOrigin().x());
+    vel_msg.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(), 2) +
+                                  pow(transform.getOrigin().y(), 2));
+    //turtle_vel.publish(vel_msg);
+    ROS_INFO("vel_msg.linear.x = %f", vel_msg.linear.x); 
+    rate.sleep();
+  }
+  return 0;
+};
