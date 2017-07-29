dd7d4e405ee1c8b308caab7e9f192be088cdbe32
yimeng_sp <lym602@126.com>
2017-07-29 15:23:46 +0300

    Heatmap: update heatmap while resource is moving

--- include/multirov/resource_heatmap.hpp
+++ include/multirov/resource_heatmap.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <geometry_msgs/Point.h>
+#include <math.h>
 
 
 // define resource heatmap class here
@@ -11,12 +12,12 @@ public:
 
 private:
 	ros::NodeHandle nodeHandle_;
-	
+
 	// Coordinates of the center of the resource vehicle, received from subscribing topic "resource_location"
 	geometry_msgs::Point resource_center; 
 
 	// An Eigen matrix, used to store heatmap
-	Eigen::MatrixXf heatmap;
+	Eigen::MatrixXi heatmap;
 
 	// Subscribing topic "resource_location"
 	// #Callback function is resource_location_Callback()	
@@ -24,4 +25,7 @@ private:
 	
 	// Call back of subscriver resource_location_subs
 	void resource_location_Callback(const geometry_msgs::Point::ConstPtr& p);
+
+	// Update heatmap while subscriber resource_location_subs receives anything (new):: now 2D version
+	void heatmap_update(int x, int y);
 };
--- src/resource_heatmap.cpp
+++ src/resource_heatmap.cpp
@@ -2,20 +2,27 @@
 #include "multirov/resource_heatmap.hpp"
 
 resource_heatmap::resource_heatmap(ros::NodeHandle& nodeHandle):nodeHandle_(nodeHandle){
-	heatmap = Eigen::MatrixXf::Zero(20, 20);
+	heatmap = Eigen::MatrixXi::Zero(200, 200);
 	resource_location_subs = nodeHandle.subscribe( "/resource_location", 5, &resource_heatmap::resource_location_Callback, this); 
 }
 
-//void: resource_heatmap::heatmap_update(){
-//	heatmap()
-//}
+void resource_heatmap::heatmap_update(int x, int y){
+	heatmap = Eigen::MatrixXi::Zero(200, 200);
+	heatmap.block(x-3, y-3, 7, 7) = Eigen::MatrixXi::Constant(7, 7, 4); 
+	heatmap.block(x-2, y-2, 5, 5) = Eigen::MatrixXi::Constant(5, 5, 6); 
+	heatmap.block(x-1, y-1, 3, 3) = Eigen::MatrixXi::Constant(3, 3, 8); 
+	heatmap(x, y) = 10;
+
+}
 
 void resource_heatmap::resource_location_Callback(const geometry_msgs::Point::ConstPtr& p){
-	resource_center.x = p->x;
-	resource_center.y = p->y;
-	resource_center.z = p->z; 
-	//heatmap_update(); 
-	ROS_INFO("center x is %f", resource_center.x); 
+	resource_center.x = (int)round(p->x);
+	resource_center.y = (int)round(p->y);
+	resource_center.z = (int)round(p->z); 
+	heatmap_update((int)round(p->x), (int)round(p->y)); 
+	ROS_INFO("center x is %f, heatmap values are %d %d %d %d", resource_center.x, heatmap((int)round(p->x), (int)round(p->y)), heatmap((int)round(p->x)+1, (int)round(p->y)+1), heatmap((int)round(p->x)+2, (int)round(p->y)+2), heatmap((int)round(p->x)+3, (int)round(p->y)+3)); 
+	Eigen::IOFormat HeavyFmt(Eigen::FullPrecision, 0, ", ", ";\n", "[", "]", "[", "]");
+	std::cout << heatmap.block((int)round(p->x)-4, (int)round(p->y)-4,9,9).format(HeavyFmt) << std::endl;
 }
 
  
