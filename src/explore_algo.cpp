9acd3aca410c305038d825ab34fba281d584aba3
yimeng_sp <lym602@126.com>
2017-08-01 20:29:48 +0300

    Explore_algo: No constraints now

--- src/explore_algo.cpp
+++ src/explore_algo.cpp
@@ -2,13 +2,19 @@
 
 
 explore_algo::explore_algo(){
-	//nearby_covermap = Eigen::MatrixXf::Zero(5, 5); 
-	//nearby_heatmap = Eigen::MatrixXi::Zero(5, 5); 
 	my_location_local.push_back(2); my_location_local.push_back(2); 
 }
 
 void explore_algo::remap_heatmap(){
-	nearby_heatmap = heatmap.block(my_location[0] - 2, my_location[1] - 2, 5, 5); // farther than 2 to the edges
+	nearby_heatmap = Eigen::MatrixXi::Zero(5, 5);
+	for(int i = -2; i < 2; i++){
+		for(int j = -2; j < 2; j++){
+			if(my_location[0] + i >= 0 & my_location[0] + i < 200 &  my_location[1] + j >= 0 &  my_location[1] + j < 200 ){
+				nearby_heatmap(i+2, j+2) = heatmap(my_location[0] + i, my_location[1] + j); 
+			}
+		}
+	}
+	// nearby_heatmap = heatmap.block(my_location[0] - 2, my_location[1] - 2, 5, 5); // farther than 2 to the edges
 	for(int j = 4; j >= 0; j--){
 		for(int i = 0; i <= 4; i++){
 			printf("%d ", heatmap(i, j)); 
@@ -34,9 +40,27 @@ void explore_algo::calculate_covermap(){
 	cm.agents.insert(cm.agents.begin(), my_location_local); 
 	std::cout << cm.agents.size() << std::endl;
 	cm.set_coveragemap(); 
+
 	for(int j = 2; j >= -2; j--){
 		for(int i = -2; i <= 2; i++){
 			printf("%f ", cm.covermap2(my_location_local[0]+i, my_location_local[1]+j)); 
+		} 
+		printf("\n");
+	}
+
+	printf("\n"); 
+
+	nearby_covermap = Eigen::MatrixXf::Zero(5, 5); 
+	for(int i = -2; i < 2; i++){
+		for(int j = -2; j < 2; j++){
+			if(my_location[0] + i >= 0 & my_location[0] + i < 200 &  my_location[1] + j >= 0 &  my_location[1] + j < 200 ){
+				nearby_covermap(i+2, j+2) = cm.covermap2(my_location_local[0] + i, my_location_local[1] + j); 
+			}
+		}
+	}
+	for(int j = 2; j >= -2; j--){
+		for(int i = -2; i <= 2; i++){
+			printf("%f ", nearby_covermap(my_location_local[0]+i, my_location_local[1]+j)); 
 		} 
 		printf("\n");
 	}
