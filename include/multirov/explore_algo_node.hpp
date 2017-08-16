ba3b84938ee14af261d08a039405ad57e2726e4b
yimeng_sp <lym602@126.com>
2017-08-16 10:58:47 +0300

    added to last commit

---
+++ include/multirov/explore_algo_node.hpp
@@ -0,0 +1,23 @@
+#ifndef ALGO_NODE
+#define ALGO_NODE
+
+#include <ros/ros.h>
+#include <geometry_msgs/Point.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int8.h>
+#include "multirov/explore_algo.hpp"
+#include "multirov/coveragemap.hpp"
+#include <string.h>
+
+class explore_algo_node{
+public: 
+
+
+};
+
+
+
+
+#endif
